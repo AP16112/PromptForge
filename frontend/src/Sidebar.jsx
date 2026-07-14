3175e6cdd5063b9d204e4ebbddca932025cb86e5
Arpit Pal <arpitpal16112@gmail.com>
2026-07-14 10:59:07 +0530

    Built the sidebar of our project

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -5,10 +5,32 @@ import "./Sidebar.css";
 
 export default function Sidebar() {
     return ( 
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            {/* PromptForge logo */}
+            <div className="promptforge-logo">
+                <a href="#"><img src="src/assets/blacklogo.png"  alt="promptforge logo"  className="logo"></img></a>
+            </div>
+
+            {/* New chat button */}
+            <button>
+                <span><i className="fa-solid fa-pen-to-square"></i> New Chat</span>
+            </button>
+
+            {/* History section */}
+            <ul className="history">
+                <li>thread 1</li>
+                <li>thread 2</li>
+                <li>thread 3</li>
+            </ul>
+
+            {/* Small Sign showing who the app belongs to */}
+            <div className="sign">
+                <p>By Arpit Pal &hearts;</p>
+            </div>
+        </section>
     );
 }
+
+
 
 
